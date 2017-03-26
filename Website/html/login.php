0610704988dc8f01fb28bccb4bcd1d9bde57bc11
LucasHokusho <lucas.farias91@yahoo.com>
2017-03-25 23:24:47 -0300

    iFit:  tela de cadastro

--- Website/html/login.php
+++ Website/html/login.php
@@ -16,10 +16,22 @@
           <h1 class="login-titulo">Login</h1>
           <input class="login-input login-email" type="text" placeholder="E-mail">
           <input class="login-input login-senha" type="password" placeholder="Senha">
+          <p class="login-texto">Cadastrar</p>
           <button class="login-botao" type="submit"></button>
       </div>
-    </section>
+     <div class="cadastro">
+          <h1 class="cadastro-titulo">Cadastro</h1>
+          <input class="cadastro-input cadastro-nome" type="text" placeholder="Nome completo">
+          <input class="cadastro-input cadastro-academia" type="text" placeholder="Nome da academia">
+          <input class="cadastro-input cadastro-idade" type="number" placeholder="Idade">
+          <input class="cadastro-input cadastro-sexo" type="text" placeholder="Sexo">
+          <input class="cadastro-input cadastro-email" type="email" placeholder="E-mail">
+          <input class="cadastro-input cadastro-senha" type="password" placeholder="Senha">
+          <p class="login-texto">Logar</p>
+          <button class="cadastro-botao" type="submit"></button>
+      </div>
+    </section>  
   </main>
   <script src="../js/navegacao.js"></script>
 </body>
-</html>
+</html> 
