7e7a0cd0b7e8e8d2669564608aed437adbbe9757
Alexis Santos <alexis.luiz.dias@gmail.com>
2017-03-31 23:48:58 -0300

    iFit: novos nomes de arquivos SASS com _ na frete menos o Import

--- Website/html/visoes/conta.php
+++ Website/html/visoes/conta.php
@@ -8,6 +8,16 @@
   </div>
  <div class="cadastro esconde">
     <h1 class="cadastro-titulo">Cadastro</h1>
+    <div class="cadastro-opcoes">
+      <div class="cadastro-opcoes-aluno">
+        <input class="cadastro-opcoes-radio" type="radio" name="cadastrarAluno" value="Aluno">
+        <p class="cadastro-opcoes-texto">Aluno</p>
+      </div>
+      <div class="cadastro-opcoes-professor">
+        <input class="cadastro-opcoes-radio" type="radio" name="cadastrarProfessor" value="Professor">
+        <p class="cadastro-opcoes-texto">Professor</p>
+      </div>
+    </div>
     <input class="cadastro-input cadastro-nome" type="text" placeholder="Nome completo">
     <input class="cadastro-input cadastro-academia" type="text" placeholder="Nome da academia">
     <input class="cadastro-input cadastro-idade" type="number" placeholder="Idade">
