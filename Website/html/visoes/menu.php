7abc426aeb2b6b49ff9e9885b4b841b110b6f9ac
LucasHokusho <lucas.farias91@yahoo.com>
2017-05-25 22:43:21 -0300

    iFit : Tela Menu

--- Website/html/visoes/menu.php
+++ Website/html/visoes/menu.php
@@ -1,9 +1,10 @@
 <section class="menu">
     <h1 class="menu-titulo">Menu</h1>
     <img src="./img/img-perfil.jpg" class="menu-foto">
-    <div>
-        <button>A</button>
-        <button>B</button>
-        <button>C</button>
+    <div class="menu-lista">
+        <button class="menu-lista-botao" type="button"><a class="menu-lista-link" href="#">Meu perfil</a></button>
+        <button class="menu-lista-botao"><a class="menu-lista-link" href="">Meus alunos</a></button>
+        <button class="menu-lista-botao"><a class="menu-lista-link" href="#">Verificar lista de solicitantes de fichas de treino</a></button>
+        <button class="menu-lista-botao"><a class="menu-lista-link" a href="#">Convidar Alunos</a></button>
     </div>
-</section> 
+</section>  
