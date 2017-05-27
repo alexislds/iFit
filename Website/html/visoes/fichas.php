2aa6ac703c4dbbcbc7fe92fcb4c69c3e2f0f8159
LucasHokusho <lucas.farias91@yahoo.com>
2017-05-26 21:31:04 -0300

    iFit: Tela de Fichas alteração margin

--- Website/html/visoes/fichas.php
+++ Website/html/visoes/fichas.php
@@ -4,9 +4,6 @@
       <button class="fichas-lista-botao" type="button">
           <a class="fichas-lista-link" href="#">Ficha de treino</a>
       </button>
-      <button class="fichas-lista-botao" type="button">
-          <a class="fichas-lista-link" href="#">Ficha de treino</a>
-      </button>
       <button class="fichas-lista-botao"><a class="fichas-lista-link" href="">Ficha de treino para quando eu tiver uma luta marcada</a></button>
       <button class="fichas-lista-botao"><a class="fichas-lista-link" href="#">Ficha de treino quando eu estiver acima do peso</a></button>
       <button class="fichas-lista-botao"><a class="fichas-lista-link" a href="#">Ficha de treino quando eu estiver acima do peso</a></button>
