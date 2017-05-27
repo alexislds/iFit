482114b6542d7822e61d2dede67dccf29ee881e2
LucasHokusho <lucas.farias91@yahoo.com>
2017-05-26 23:15:54 -0300

    iFit : Tela Aluno/Especificacoes

--- Website/html/visoes/aluno.php
+++ Website/html/visoes/aluno.php
@@ -1,14 +1,32 @@
-<section>
-    <div>
-        
-        
-        
-        
+<section class="aluno">
+    <h1 class="aluno-titulo">Aluno</h1>
+    <div class="aluno-especificacoes">
+        <h1 class="aluno-especificacoes-titulo">Especificacoes</h1>
+        <form action="#" method="post" class="especificacoes-form">
+            <div class="aluno-especificacoes-lista">
+                <input class="aluno-especificacoes-lista-input" type="text" placeholder="teste">
+                <input class="aluno-especificacoes-lista-input" type="text" placeholder="teste">
+                <input class="aluno-especificacoes-lista-input" type="text" placeholder="teste">
+                <input class="aluno-especificacoes-lista-input" type="text" placeholder="teste">
+                <input class="aluno-especificacoes-lista-input" type="text" placeholder="teste">
+                <input class="aluno-especificacoes-lista-input" type="text" placeholder="teste">
+                <input class="aluno-especificacoes-lista-input" type="text" placeholder="teste">
+            </div>
+        </form>
     </div>
-    <div>
-        
-        
-        
-        
+    <div class="aluno-fichas">
+         <h1 class="aluno-fichas-titulo">Fichas</h1>
+        <div class="aluno-fichas-lista">
+            <button class="aluno-fichas-lista-botao" type="button"> <a class="aluno-fichas-lista-link" href="#">Ficha de treino</a> </button>
+            <button class="aluno-fichas-lista-botao"><a class="aluno-fichas-lista-link" href="">Ficha de treino para quando eu tiver uma luta marcada</a></button>
+            <button class="aluno-fichas-lista-botao"><a class="aluno-fichas-lista-link" href="#">Ficha de treino quando eu estiver acima do peso</a></button>
+            <button class="aluno-fichas-lista-botao"><a class="aluno-fichas-lista-link" a href="#">Ficha de treino quando eu estiver acima do peso</a></button>
+            <button class="aluno-fichas-lista-botao"><a class="aluno-fichas-lista-link" a href="#">Ficha de treino</a></button>
+            <button class="aluno-fichas-lista-botao" type="button"><a class="aluno-fichas-lista-link" href="#">Ficha de treino</a></button>
+            <button class="aluno-fichas-lista-botao"><a class="aluno-fichas-lista-link" href="">Ficha de treino para quando eu tiver uma luta marcada</a></button>
+            <button class="aluno-fichas-lista-botao"><a class="aluno-fichas-lista-link" href="#">Ficha de treino quando eu estiver acima do peso</a></button>
+            <button class="aluno-fichas-lista-botao"><a class="aluno-fichas-lista-link" a href="#">Ficha de treino quando eu estiver acima do peso</a></button>
+            <button class="aluno-fichas-lista-botao"><a class="aluno-fichas-lista-link" a href="#">Ficha de treino</a></button>
+        </div>
     </div>
 </section>
