c0f54b020e156023222f7662b02d941686047e2c
LucasHokusho <lucas.farias91@yahoo.com>
2017-06-08 18:53:32 -0300

    iFit : Tela de perfil

--- Website/html/visoes/perfil.php
+++ Website/html/visoes/perfil.php
@@ -1,13 +1,58 @@
+<?php
+
+    if(isset($_SESSION['id_usuario'])){
+        $id = "'" . $_SESSION['id_usuario'] . "'";
+
+        $resultado = $conexao->query("
+        SELECT email, senha, nome, academia, sexo, idade FROM USUARIO WHERE id_usuario = $id;
+        ")->fetch_assoc();
+
+        $email = $resultado['email'];
+        $senha = $resultado['senha'];
+        $nome = $resultado['nome'];
+        $academia = $resultado['academia'];
+        $sexo = $resultado['sexo'];
+        $idade = $resultado['idade'];
+    }
+    
+?>
 <section class="perfil">
     <h1 class="perfil-titulo">Perfil</h1>
     <form action="#" method="post" class="perfil-form">
         <div class="perfil-lista">
-            <input class="perfil-lista-input" type="text" placeholder="teste">
-            <input class="perfil-lista-input" type="text" placeholder="teste">
-            <input class="perfil-lista-input" type="text" placeholder="teste">
-            <input class="perfil-lista-input" type="text" placeholder="teste">
-            <input class="perfil-lista-input" type="text" placeholder="teste">
-            <input class="perfil-lista-input" type="text" placeholder="teste">
+            <?php 
+                if(isset($_SESSION['id_usuario'])){
+                    echo "<div class='perfil-lista-item'>";
+                    echo "<label class='perfil-lista-label'>Seu email:</label>";
+                    echo "<input class='perfil-lista-input' type='text' placeholder='Seu email:' value='$email' disabled>";
+                    echo "</div>";
+
+                    echo "<div class='perfil-lista-item' >";
+                    echo "<label class='perfil-lista-label'>Sua senha:</label>";
+                    echo "<input class='perfil-lista-input' type='text' placeholder='Sua senha:'  value='$senha' disabled>";
+                    echo "</div>";
+
+                    echo "<div class='perfil-lista-item'>";
+                    echo "<label class='perfil-lista-label'>Seu nome:</label>";
+                    echo "<input class='perfil-lista-input' type='text' placeholder='Seu nome:'  value='$nome' disabled>";
+                    echo "</div>";
+
+                    echo "<div class='perfil-lista-item'>";
+                    echo "<label class='perfil-lista-label'>Sua academia:</label>";
+                    echo "<input class='perfil-lista-input' type='text' placeholder='Sua academia'  value='$academia' disabled>";
+                    echo "</div>";
+
+                    echo "<div class='perfil-lista-item'>";
+                    echo "<label class='perfil-lista-label'>Sexo:</label>";
+                    echo "<input class='perfil-lista-input' type='text' placeholder='Seu sexo'  value='$sexo' disabled>";
+                    echo "</div>";
+
+                    echo "<div class='perfil-lista-item'>";
+                    echo "<label class='perfil-lista-label'>Sua idade:</label>";
+                    echo "<input class='perfil-lista-input' type='text' placeholder='Sua idade'  value='$idade' disabled>";
+                    echo "</div>";
+                }
+            ?>
             <div class="perfil-lista-botoes">
                 <button class="perfil-lista-editar" type="button">Editar</button>
                 <button class="perfil-lista-salvar" type="submit">Salvar</button>
