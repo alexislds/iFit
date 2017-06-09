bd1e8dc05cbbd592e26356ed5df047f3b5f27b0e
LucasHokusho <lucas.farias91@yahoo.com>
2017-06-08 21:02:22 -0300

    iFit : Solicitar Ficha

--- Website/html/visoes/solicitar-ficha.php
+++ Website/html/visoes/solicitar-ficha.php
@@ -2,8 +2,18 @@
     <h1 class="solicita-ficha-titulo">Solicitação de Ficha</h1>
     <form action="#" method="post" class="solicita-ficha-form">
         <div class="solicita-ficha-lista">
-            <input class="solicita-ficha-lista-input" type="text" placeholder="teste">
-            <input class="solicita-ficha-lista-input" type="text" placeholder="teste">
+        <?php
+           if(isset($_SESSION['id_usuario'])){
+            echo "<div class='solicita-ficha-lista-item'>";
+            echo "<label class='solicita-ficha-lista-label'>Voce gostaria de um professor com especialidade em que area? </label>";
+            echo "<input class='solicita-ficha-lista-input' type='text' placeholder='Voce gostaria de um professor com especialidade em que area?'>";
+            
+            echo "<div class='solicita-ficha-lista-item'>";
+            echo "<label class='solicita-ficha-lista-label'>Descreva brevemente o que necessita em sua ficha de treino, quanto mais melhor.</label>";
+            echo "<input class='solicita-ficha-lista-input' type='text' placeholder='Descreva brevemente o que necessita em sua ficha de treino, quanto mais melhor.'>";
+            
+           }; 
+        ?>    
             <div class="solicita-ficha-lista-botoes">
                 <button class="solicita-ficha-lista-enviar" type="submit">Enviar</button>
             </div>
